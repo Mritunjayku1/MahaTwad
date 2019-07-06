36b6c2793577dbf390a72ab4292c0144cdfdf124
mritunjayku1 <mritunjayku1@gmail.com>
2019-07-06 21:22:51 +0530

    bug fixing

--- TWAD-SERVICE/src/com/water/util/StatusConstant.java
+++ TWAD-SERVICE/src/com/water/util/StatusConstant.java
@@ -4,14 +4,14 @@ public class StatusConstant {
 
 	public static final String HOUSER_ASSIGN_OFFICE="Assigning application to EE User";
 	public static final String EEUSER_PENDING_APPLICATION_FEE="Application fee not paid by User";
-	public static final String EEUSER_PAID_APPLICATION_FEE="Application fee paid, Waiting for DD and Aproval ";
-	public static final String EEUSER_APPROVED_APPLICATION_FEE="Application fee Approved, DD Received";
+	public static final String EEUSER_PAID_APPLICATION_FEE="Application fee paid, Waiting for Payment Verification ";
+	public static final String EEUSER_APPROVED_APPLICATION_FEE="Application fee Approved, Payment Verified";
 	public static final String EEUSER_PENDING_UPFRONT_CHARGES="Upfront Charges not paid by User";
-	public static final String EEUSER_PAID_UPFRONT_CHARGES="Upfront Charges paid, Waiting for DD and Aproval ";
-	public static final String EEUSER_APPROVED_UPFRONT_CHARGES="Upfront Charges Approved, DD Received";
+	public static final String EEUSER_PAID_UPFRONT_CHARGES="Upfront Charges paid, Waiting for Payment Verification ";
+	public static final String EEUSER_APPROVED_UPFRONT_CHARGES="Upfront Charges Approved, Payment Verified";
 	public static final String EEUSER_PENDING_FULL_PAYMENT="Full Payment not paid by user";
-	public static final String EEUSER_PAID_FULL_PAYMENT="Full Payment paid, Waiting for DD and Aproval";
-	public static final String EEUSER_APPROVED_FULL_PAYMENT="Full Payment Approved, DD Received";
+	public static final String EEUSER_PAID_FULL_PAYMENT="Full Payment paid, Waiting for Payment Verification";
+	public static final String EEUSER_APPROVED_FULL_PAYMENT="Full Payment Approved, Payment Verified";
 	public static final String EEUSER_WAITING_INSPECTION="Inspection in Progress";
 	public static final String MCUSER_PENDING_FULL_PAYMENT_APPROVAL="Full Payment Amount waiting for Approval by MC User";
 	public static final String MCUSER_APPROVED_FULL_PAYMENT="Full Payment Amount Approved by MC User";
